29a7ff733a3fe78f7389a1f6cf156486260bece4
divyanshbramhe <divyanshbramhe007@gmail.com>
2026-05-09 13:27:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
+# =========================
+# LOAD DATASET
+# =========================
+
+df = pd.read_csv("data/movies.csv")
+
+# =========================
+# ROOT
+# =========================
+
 @app.get("/")
 def home():
-    return {"message": "API is running"}
+
+    return {
+        "message": "Dataset loaded successfully",
+        "movies": len(df)
+    }
