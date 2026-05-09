9ec1e2f2775e7b2b659dd3bf236fa22cf9e628c0
divyanshbramhe <divyanshbramhe007@gmail.com>
2026-05-09 13:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from recommender.engine import recommend
-
-# =========================
-# APP
-# =========================
 
 app = FastAPI()
 
-# =========================
-# CORS
-# =========================
-
-app.add_middleware(
-
-    CORSMiddleware,
-
-    allow_origins=["*"],
-
-    allow_credentials=True,
-
-    allow_methods=["*"],
-
-    allow_headers=["*"],
-)
-
-# =========================
-# ROOT
-# =========================
-
 @app.get("/")
-
 def home():
-
-    return {
-        "message": "Netflix Buddy API Running"
-    }
-
-# =========================
-# ASK ENDPOINT
-# =========================
-
-@app.get("/ask")
-
-def ask(q: str):
-
-    result = recommend(q)
-
-    return result
+    return {"message": "API is running"}
